cda7cbcf30b1ea4b8c2be2459c5e1c0b31b70001
Steph <stephanetapascom@hotmail.com>
2022-12-28 15:36:48 +0100

    extratct web test1

--- bin/extract.c
+++ bin/extract.c
@@ -13,7 +13,7 @@ int main(void)
     curl = curl_easy_init();
     if(curl) {
         curl_easy_setopt(curl, CURLOPT_URL, "https://www.example.com/page-web");
-        res = curl_easy_perform(curl);
+        res = curl_easy_perform(curl); //res est une variable de type CURLcode qui stocke le résultat de l'opération cURL.
         if(res != CURLE_OK) {
             fprintf(stderr, "Erreur lors de la récupération de la page web : %s\n",
                     curl_easy_strerror(res));
